12a24e14f62ca11c22cde3d8bf2c56fbc4d9a837
Haritz Medina <haritzmedina@gmail.com>
2016-12-02 12:26:32 +0100

    Repaired css and play/pause button. Added jquery as lib. Two radio stations added. Other minimum changes.

---
+++ js/default.js
@@ -0,0 +1,73 @@
+(function(){
+
+    function addEventListener(listenButton){
+        listenButton.addEventListener("click", function(event){
+            // Get clicked radio
+            var radioUri = event.toElement.dataset.radioUri;
+            // Get player
+            var player = document.getElementById('audioPlayer');
+            // Set new radio to the player
+            player.innerHTML = "<source id='sourcePL' src='"+radioUri+"' type='audio/mpeg'/>";
+            // Reload and play
+            player.load();
+            playPlayer();
+            // Set to select current radio
+            selectRadio(event.toElement);
+        });
+    }
+
+    function setControlsListeners(){
+        var playPauseButton = document.getElementById('player-play-pause');
+        playPauseButton.addEventListener('click', function(){
+            var player = document.getElementById('audioPlayer');
+            if(player.paused){
+                playPlayer();
+            }
+            else{
+                pausePlayer();
+            }
+        });
+    }
+
+    function selectRadio(elem){
+        deselectRadios();
+        elem.dataset.selected = "true";
+    }
+
+    function deselectRadios(){
+        var listenButtons = document.getElementsByClassName("listenRadio");
+        for(i=0; i< listenButtons.length; i++){
+            listenButtons[i].dataset.selected = "false";
+        }
+    }
+
+    function pausePlayer(){
+        var player = document.getElementById('audioPlayer');
+        if(player.firstChild.nodeName==='SOURCE'){
+            player.pause();
+            // Change control icon
+            var playPauseButton = document.getElementById('player-play-pause');
+            playPauseButton.innerHTML = '<img src="img/play.png"/>';
+        }
+    }
+
+    function playPlayer(){
+        var player = document.getElementById('audioPlayer');
+        if(player.firstChild.nodeName==='SOURCE'){
+            player.play();
+            // Change control icon
+            var playPauseButton = document.getElementById('player-play-pause');
+            playPauseButton.innerHTML = '<img src="img/pause.png"/>';
+        }
+    }
+
+
+    var listenButtons = document.getElementsByClassName("listenRadio");
+    var i = 0;
+    for(i=0; i<listenButtons.length; i++){
+        addEventListener(listenButtons[i]);
+    }
+
+    setControlsListeners();
+
+})();
